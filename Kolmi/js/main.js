8248896f58150622d450217a1aa7a5508a37d239
no-loneliness <lonely31271995@gmail.com>
2017-11-23 00:21:38 +0300

    Add files via upload

--- Kolmi/js/main.js
+++ Kolmi/js/main.js
@@ -1,34 +1,4 @@
-$(window).load(function() {
-    setTimeout(function() {
-        $(".loader").fadeOut();
-        $("html").fadeIn();
 
-        UIkit.util.on('.uk-slideshow', 'itemshown', function() {
-            $(".spincrement").spincrement({
-                from: 0, // Стартовое число
-                to: false, // Итоговое число. Если false, то число будет браться из элемента с классом spincrement, также сюда можно напрямую прописать число. При этом оно может быть, как целым, так и с плавающей запятой
-                decimalPlaces: 0, // Сколько знаков оставлять после запятой
-                decimalPoint: ".", // Разделитель десятичной части числа
-                thousandSeparator: " ", // Разделитель тыcячных
-                duration: 2000, // Продолжительность анимации в миллисекундах
-                leeway: 0, // погрешность продолжительности анимации в процентах
-                easing: "easeOutCirc",
-            });
-        });
-
-        $(".spincrement").spincrement({
-            from: 0, // Стартовое число
-            to: false, // Итоговое число. Если false, то число будет браться из элемента с классом spincrement, также сюда можно напрямую прописать число. При этом оно может быть, как целым, так и с плавающей запятой
-            decimalPlaces: 0, // Сколько знаков оставлять после запятой
-            decimalPoint: ".", // Разделитель десятичной части числа
-            thousandSeparator: " ", // Разделитель тыcячных
-            duration: 2000, // Продолжительность анимации в миллисекундах
-            leeway: 0, // погрешность продолжительности анимации в процентах
-            easing: "easeOutCirc",
-        });
-
-    }, 2500);
-});
 
 // отрисовка линий svg-лого
 var lineDrawing = anime({
@@ -48,28 +18,4 @@ $(function() {
 
     // Custom Easing
     jQuery.scrollSpeed(100, 800, 'easeOutCubic');
-});
-
-// слайдер логотипов
-$(document).ready(function() {
-    $('.customer-logos').slick({
-        slidesToShow: 4,
-        slidesToScroll: 2,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        arrows: false,
-        dots: false,
-        pauseOnHover: true,
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 3
-            }
-        }, {
-            breakpoint: 520,
-            settings: {
-                slidesToShow: 2
-            }
-        }]
-    });
 });
