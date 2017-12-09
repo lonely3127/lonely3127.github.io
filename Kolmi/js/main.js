687a0fb223c47ae2dcd7764617564113391e78a3
no-loneliness <lonely31271995@gmail.com>
2017-12-09 22:56:06 +0300

    Add files via upload

--- Kolmi/js/main.js
+++ Kolmi/js/main.js
@@ -1,16 +1,5 @@
 
 
-// отрисовка линий svg-лого
-var lineDrawing = anime({
-    targets: '#lineDrawing .lines path',
-    strokeDashoffset: [anime.setDashoffset, 0],
-    easing: 'easeInOutSine',
-    duration: 1500,
-    delay: function(el, i) { return i * 450 },
-    direction: 'alternate',
-    loop: true
-});
-
 // плавный скроллинг
 $(function() {
     // Default
